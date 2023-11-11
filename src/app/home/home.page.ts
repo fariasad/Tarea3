73916cee3695047ef0db8916c8daa4da99194d69
fariasad <fariasad@gmail.com>
2023-11-11 16:30:50 -0300

    Configuración de menú

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle],
 })
 export class HomePage {
   constructor() {}
